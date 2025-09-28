3c62b321b620efdab71962006ef804c740859bac
alwaysal0 <alwaysal0dev@gmail.com>
2025-09-28 21:13:37 +0300

    Restyling Support Page and Product card

--- resources/views/modules/profile/product-card.blade.php
+++ resources/views/modules/profile/product-card.blade.php
@@ -5,4 +5,14 @@
     <p><span>ID:</span> {{ $product->id }}</p>
     <p><span>Name:</span> {{ $product->name }}</p>
     <p class="description"><span>Description:</span> {{ $product->description }}</p>
+    <div class="profile-right-cont-product-card-price-buy"">
+        <p><span>Price:</span>{{ $product->price }}</p>
+        <form method="POST" action="">
+            @if(request()->is('profile/your-products'))
+                <button type="submit" class="disabled-product-card" disabled>Buy</button>
+            @else
+                <button type="submit">Buy</button>
+            @endif
+        </form>
+    </div>
 </div>
