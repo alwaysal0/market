21ac16613f19ab9f0ab3ca98cada10751c7d507c
alwaysal0 <alwaysal0dev@gmail.com>
2025-12-23 18:49:25 +0200

    feat: Implemented new methods for increment/decrement actions

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Product;
 
 class CartController extends Controller
 {
-    public function add(Request $request, Product $product) {
+    /*
+     * Adds new product data to a nested "cart" array in the session.
+     * Redirects the user back to the CartPage.
+     */
+    public function add(Product $product) {
         $cart = session()->get('cart');
         if (isset($cart[$product->id])) {
             $cart[$product->id]['quantity']++;
         } else {
             $cart[$product->id] = [
+                'id_product' => $product->id,
                 'name' => $product->name,
                 'price' => $product->price,
                 'quantity' => 1,
@@ -24,8 +28,14 @@ class CartController extends Controller
         return redirect()->back()->with('success', 'Product added to cart successfully!');
     }
 
-    public function remove(Request $request, Product $product) {
-        if ($product->id) {
+    /*
+     * Decrements the product quantity in the cart array.
+     * Redirects the user back to the CartPage.
+     */
+    public function decrease(String $id_product) {
+        $id_product = (int) $id_product;
+        $product = Product::findOrFail($id_product);
+        if ($product) {
             $cart = session()->get('cart');
             if (isset($cart[$product->id])) {
                 $cart[$product->id]['quantity']--;
@@ -33,8 +43,27 @@ class CartController extends Controller
                     unset($cart[$product->id]);
                 }
                 session()->put('cart', $cart);
-                session()->flash('success', 'The product has been removed.');
             }
         }
+
+        return redirect()->back()->with('success', 'The product quantity has been decreased.');
+    }
+
+    /*
+     * Increments the product quantity in the cart array.
+     * Redirects the user back to the CartPage.
+     */
+    public function increase(String $id_product) {
+        $id_product = (int) $id_product;
+        $product = Product::findOrFail($id_product);
+        if ($product) {
+            $cart = session()->get('cart');
+            if (isset($cart[$product->id])) {
+                $cart[$product->id]['quantity']++;
+                session()->put('cart', $cart);
+            }
+        }
+
+        return redirect()->back()->with('success', 'The product quantity has been increased.');
     }
 }
