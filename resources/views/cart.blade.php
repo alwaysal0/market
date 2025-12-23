4192af75a348f5f5c6ebfd4cab2b9975605e6198
alwaysal0 <alwaysal0dev@gmail.com>
2025-12-23 18:50:15 +0200

    feat: Fixed content and style of page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Market - cart')
 @section('content')
 
+<div id="cart-cont">
 @if(session('cart'))
     <table id="cart-table">
         <thead>
@@ -19,28 +20,49 @@
                 <tr class="cart-product-card" id="{{ $id }}">
                     <td class="col-image"><img src="{{ $product['image'] }}" alt="image-{{ $id }}"></td>
                     <td class="col-name">{{ $product['name'] }}</td>
-                    <td class="col-price">{{ $product['price'] }}</td>
-                    <td class="col-quantity">{{ $product['quantity'] }}</td>
+                    <td class="col-price">{{ $product['price'] }}$</td>
+                    <td class="col-quantity">
+                        <div class="quantity-control">
+                            <span class="qty-display">{{ $product['quantity'] }}</span>
+                            <div class="qty-buttons">
+                                <a href="{{ route('cart.increase', ['id_product' => $product['id_product']]) }}" class="btn-qty increase">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#303030"><path d="m280-400 200-200 200 200H280Z"/></svg>
+                                </a>
+                                <a href="{{ route('cart.decrease', ['id_product' => $product['id_product']]) }}" class="btn-qty decrease">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#303030"><path d="M480-360 280-560h400L480-360Z"/></svg>
+                                </a>
+                            </div>
+                        </div>
+                    </td>
                 </tr>
             @endforeach
             <tr>
-                <td colspan="4">Total price:<span id="final-price">0</span></td>
+                <td colspan="4">Total price: <span id="final-price">0</span>$</td>
             </tr>
         </tbody>
     </table>
+    <form action="#">
+        <button type="submit" id="cart-to-order__button">To Order</button>
+    </form>
 @else
-    Empty cart.
+    <p id="cart-empty__title">Empty Cart</p>
+    <p id="cart-empty__description">Add new products to your cart and they will appear here.</p>
 @endif
+</div>
+
 <script>
     document.addEventListener('DOMContentLoaded', () => {
         const FINAL_FIELD = document.getElementById('final-price');
         let products = document.querySelectorAll('.cart-product-card');
         let final_cost = 0;
         products.forEach(product => {
-            final_cost += parseInt(product.querySelector(".col-price").innerText);
+            let current_price = parseInt(product.querySelector('.col-price').innerText);
+            let current_quantity = parseInt(product.querySelector('.qty-display').innerText);
+            final_cost += current_price * current_quantity;
         })
 
         FINAL_FIELD.innerHTML = final_cost;
     })
 </script>
+
 @endsection
